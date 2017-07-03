4ecee81632889dedf7195fe3441de014dccd5d65
jackfowl <jacksonluizdemarco@gmail.com>
2017-07-03 20:50:50 -0300

    Initial commit

---
+++ TesteReconhecimento/HaarClassifier.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using Emgu.CV.CvEnum;
+using System.IO;
+using Emgu.CV.Util;
+
+namespace TesteReconhecimento
+{
+    class HaarClassifier
+    {
+        private CascadeClassifier _Classifier;
+        private string _HaarFile;
+
+        public string HaarFile
+        {
+            get { return _HaarFile; }
+            set
+            {
+                _HaarFile = value;
+                _Classifier = new CascadeClassifier(value);
+            }
+        }
+        public double Scale { get; set; }
+        public int Neighboors { get; set; }
+        public Size TrainedSize { get; set; }
+
+        public HaarClassifier() {
+            Scale = 1.1;
+            Neighboors = 3;
+            TrainedSize = Size.Empty; }
+
+        private MemoryStream DrawMatches(Mat mat)
+        {
+
+            Rectangle[] matches;
+            using (UMat img = mat.GetUMat(AccessType.ReadWrite))
+                matches = GetMatches(img);
+
+            foreach (Rectangle rect in matches)
+                CvInvoke.Rectangle(mat, rect, new MCvScalar(0, 0, 255), 2);
+
+            MemoryStream result = null;
+            using (VectorOfByte vb = new VectorOfByte())
+            {
+                CvInvoke.Imencode(".jpg", mat, vb);
+                byte[] rawData = vb.ToArray();
+                result = new MemoryStream(rawData);
+            }
+
+            return result;
+        }
+
+        public async Task<MemoryStream> GetImageWithMatches(string file)
+        {
+            Mat mat = CvInvoke.Imread(file, ImreadModes.Color);
+            Task<MemoryStream> t = new Task<MemoryStream>(() => DrawMatches(mat));
+            t.Start();
+            var taskResult = await t;
+            return taskResult;
+        }
+
+        public async Task<MemoryStream> GetImageWithMatches(MemoryStream image)
+        {
+            byte[] data = image.ToArray();
+            Mat mat = new Mat();
+            CvInvoke.Imdecode(data, ImreadModes.Color, mat);
+            Task<MemoryStream> t = new Task<MemoryStream>(() => DrawMatches(mat));
+            t.Start();
+            var taskResult = await t;
+            return taskResult;
+        }
+
+        public Rectangle[] GetMatches(string file)
+        {
+            Mat mat = CvInvoke.Imread(file, ImreadModes.Color);
+            Rectangle[] matches;
+            using (UMat img = mat.GetUMat(AccessType.ReadWrite))
+                matches = GetMatches(img);
+
+            return matches;
+        }
+        public Rectangle[] GetMatches(IInputArray image)
+        {
+            using (UMat ugray = new UMat())
+            {
+                //grayscale
+                CvInvoke.CvtColor(image, ugray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+
+                //normalizes brightness and increases contrast of the image
+                CvInvoke.EqualizeHist(ugray, ugray);
+
+                return _Classifier.DetectMultiScale(ugray, Scale, Neighboors, TrainedSize);
+            }
+        }
+    }
+}
---
+++ TesteReconhecimento/MainPage.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Windows.ApplicationModel.Core;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Windows.UI.Core;
+
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace TesteReconhecimento
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private VideoCapture _capture;
+        private bool _CaptureEnabled;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FillClassifiers();
+            FillImages();
+            Window.Current.VisibilityChanged += (sender, args) =>
+            {
+                CvInvoke.WinrtOnVisibilityChanged(args.Visible);
+            };
+            
+            CvInvoke.WinrtSetFrameContainer(this.imgFinal);
+
+            CvInvoke.WinrtStartMessageLoop(Process);
+
+        }
+
+        private async void FillImages()
+        {
+            cbImages = (ComboBox)FindName("cbImages");
+            cbImages.SelectedValuePath = "Name";
+            StorageFolder appInstalledFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFolder assets = await appInstalledFolder.GetFolderAsync("Images");
+            var files = await assets.GetFilesAsync();
+            cbImages.ItemsSource = files;
+        }
+
+        private async void FillClassifiers()
+        {
+            cbClassifier = (ComboBox)FindName("cbClassifier");
+            cbClassifier.SelectedValuePath = "Name";
+            StorageFolder appInstalledFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFolder assets = await appInstalledFolder.GetFolderAsync("Classifiers");
+            var files = await assets.GetFilesAsync();
+            cbClassifier.ItemsSource = files;
+        }
+
+        private async void btnReconhecer_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HaarClassifier c = new HaarClassifier();
+                c.HaarFile = "Classifiers/" + cbClassifier.SelectedValue.ToString();
+                c.Scale = Convert.ToSingle(txtScale.Text);
+                c.Neighboors = Convert.ToInt32(txtNeighboors.Text);
+                BitmapImage i = new BitmapImage();
+                var ms = await c.GetImageWithMatches("Images/" + cbImages.SelectedValue.ToString());
+                await i.SetSourceAsync(ms.AsRandomAccessStream());
+                imgFinal.Source = i;
+            }
+            catch (Exception except)
+            {
+                MessageDialog showDialog = new MessageDialog(except.Message);
+                await showDialog.ShowAsync();
+            }
+        }
+
+        private void Process()
+        {
+            Mat m = new Mat();
+            Mat mProcessed = new Mat();
+            while (true)
+            {
+                if (_CaptureEnabled)
+                {
+                    try
+                    {
+                        if (_capture == null)
+                            _capture = new VideoCapture();
+
+                        _capture.Read(m); //WHY EMPTY
+
+                        if (!m.IsEmpty)
+                        {
+                            //render the processed image on the top imageview
+                            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                                async () =>
+                                {
+                                    var wb = mProcessed.ToWritableBitmap();
+                                    imgFinal.Source = wb;
+                                });
+
+                            //The data in the mat that is read from the camera will 
+                            //be drawn to the Image control
+                            CvInvoke.WinrtImshow();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e.Message);
+                    }
+                    _CaptureEnabled = false;
+                }
+                else
+                {
+                    if (_capture != null)
+                    {
+                        _capture.Dispose();
+                        _capture = null;
+                    }
+
+                    Task t = Task.Delay(100);
+                    t.Wait();
+                }
+            }
+        }
+
+        private void btnGetCamera_ClickAsync(object sender, RoutedEventArgs e)
+        {
+
+            _CaptureEnabled = true;
+        }
+    }
+}
